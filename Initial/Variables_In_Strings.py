9fab0083c3165ec7695a0a92f5dd86d14329e9e4
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-03 11:45:31 +0545

    About using strings in variables.

--- Initial/Variables_In_Strings.py
+++ Initial/Variables_In_Strings.py
@@ -3,3 +3,5 @@ name = "Eric"
 surname = "Matthes"
 full_name = f"{name} {surname}"
 print(f"Hello, {full_name.title()}!")
+
+# Note: f-string is a new feature in Python 3.6 if you are using below version you should use format() method. like full_name = "{} {}".format(name, surname)
