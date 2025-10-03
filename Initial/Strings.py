ea0000dc869b61d288b2cd405fc29352a2682c79
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-03 09:49:52 +0545

    Some basic operations on strings and string recognition.

---
+++ Initial/Strings.py
@@ -0,0 +1,16 @@
+#Strings are the text inside quotes.
+message1 = "This is the string inside the double quotes."
+message2 = 'This is also the string inside the single quotes.'
+print(message1)
+print(message2)
+
+# TO check and verify the type of the variable message1 and message2
+print(type(message1))
+print(type(message2))
+
+# Changing Case instrings with methods.
+
+name = "eric matthes"
+print(name.title()) # Changes the name to title case where the first letter is capital.
+print(name.upper()) # Changes the name to upper case
+print(name.lower()) # Changes the name to lower case
