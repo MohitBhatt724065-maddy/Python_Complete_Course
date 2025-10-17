595a17816d89630a23555bb47ddf64c30006f062
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-17 14:05:13 +0545

    Created value using list comprehension on coordinates and unpacked the tuple coordinate from list and visualized plot using matplotlib library.

--- List_Comprehension/Second.py
+++ List_Comprehension/Second.py
@@ -1,1 +1,16 @@
-#
+# Using Nested loop.
+value = [(x,y) for x in range(5) for y in range(5)]
+print(value)
+
+# Extracting x and y from value.
+x_cord = [x for x , y in value]
+y_cord = [y for x, y in value]
+print(x_cord)
+print(y_cord)
+
+# Visualization using scatter and line plot using matplotlib.
+import matplotlib.pyplot as plt
+
+plt.scatter(x_cord, y_cord)
+plt.plot(x_cord, y_cord)
+plt.show()
